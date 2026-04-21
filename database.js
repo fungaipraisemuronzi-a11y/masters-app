aa448c882fbaaa141a9a54057e46a4aeb29d0c90
Fungayi <fungaipraisemuronzi@gmail.com>
2026-04-21 12:38:30 +0200

    Full Neon conversion fix

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_REwzGgXZ91KH@ep-old-bar-a48098ou-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
